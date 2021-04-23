ce780baa94589854967c4ee79f261c0e1a78b5fe
MajeedKazemi <majeed@cs.toronto.edu>
2021-04-23 16:04:15 +0430

    added unsubscribe function with a callerId to unsubscribe a particular item from the callbacks of a code construct

--- src/editor/hole.ts
+++ src/editor/hole.ts
@@ -1,66 +1,80 @@
-import { CallbackType, CodeConstruct } from "../syntax-tree/ast";
-import Editor from "./editor";
+import { CallbackType, CodeConstruct, Callback } from '../syntax-tree/ast';
+import Editor from './editor';
 
 export default class Hole {
-    element: HTMLDivElement;
-    editor: Editor;
-    code: CodeConstruct;
+	element: HTMLDivElement;
+	editor: Editor;
+	code: CodeConstruct;
 
-    constructor(editor: Editor, code: CodeConstruct) {
-        this.editor = editor;
-        this.code = code;
+	constructor(editor: Editor, code: CodeConstruct) {
+		this.editor = editor;
+		this.code = code;
 
-        // Add monaco padding
+		// Add monaco padding
 
-        // Dom element
-        const element = document.createElement("div");
-        element.classList.add('hole');
-        document.body.append(element);
-        this.element = element;
+		// Dom element
+		const element = document.createElement('div');
+		element.classList.add('hole');
+		document.body.append(element);
+		this.element = element;
 
-        const hole = this;
-        
-        code.subscribe(CallbackType.change, () => {
-            const bbox = editor.computeBoundingBox(code.getSelection());
-            if (bbox.width == 0) {
-                bbox.x -= 4;
-                bbox.width = 8;
-            }
-            hole.setTransform(bbox);
-        });
+		const hole = this;
 
-        code.subscribe(CallbackType.delete, () => {
-            hole.setTransform({x: 0, y: 0, width: 0, height: 0});
-            hole.element.remove();
-        });
-        
-        code.subscribe(CallbackType.replace, () => {
-            console.log("Replaced");
-            hole.setTransform({x: 0, y: 0, width: 0, height: 0});
-        });
+		code.subscribe(
+			CallbackType.change,
+			new Callback(() => {
+				const bbox = editor.computeBoundingBox(code.getSelection());
 
-        code.subscribe(CallbackType.fail, () => {
-            hole.element.style.background = `rgba(255, 0, 0, 0.06)`;
-            setTimeout(() => {
-                hole.element.style.background = `rgba(255, 0, 0, 0)`;
-            }, 1000);
-        });
+				if (bbox.width == 0) {
+					bbox.x -= 4;
+					bbox.width = 8;
+				}
 
-        const bbox = editor.computeBoundingBox(code.getSelection());
-        if (bbox.width == 0) {
-            bbox.x -= 4;
-            bbox.width = 8;
-        }
-        hole.setTransform(bbox);
-    }
+				hole.setTransform(bbox);
+			})
+		);
 
-    setTransform(transform: { x: number; width: number; y: number; height: number; }) {
-        const padding = 5;
-    
-        this.element.style.top = `${transform.y - padding}px`;
-        this.element.style.left = `${transform.x - padding}px`;
+		code.subscribe(
+			CallbackType.delete,
+			new Callback(() => {
+				hole.setTransform({ x: 0, y: 0, width: 0, height: 0 });
+				hole.element.remove();
+			})
+		);
 
-        this.element.style.width = `${transform.width + padding * 2}px`;
-        this.element.style.height = `${transform.height + padding * 2}px`;
-    }
+		code.subscribe(
+			CallbackType.replace,
+			new Callback(() => {
+				console.log('Replaced');
+				hole.setTransform({ x: 0, y: 0, width: 0, height: 0 });
+			})
+		);
+
+		code.subscribe(
+			CallbackType.fail,
+			new Callback(() => {
+				hole.element.style.background = `rgba(255, 0, 0, 0.06)`;
+				setTimeout(() => {
+					hole.element.style.background = `rgba(255, 0, 0, 0)`;
+				}, 1000);
+			})
+		);
+
+		const bbox = editor.computeBoundingBox(code.getSelection());
+		if (bbox.width == 0) {
+			bbox.x -= 4;
+			bbox.width = 8;
+		}
+		hole.setTransform(bbox);
+	}
+
+	setTransform(transform: { x: number; width: number; y: number; height: number }) {
+		const padding = 5;
+
+		this.element.style.top = `${transform.y - padding}px`;
+		this.element.style.left = `${transform.x - padding}px`;
+
+		this.element.style.width = `${transform.width + padding * 2}px`;
+		this.element.style.height = `${transform.height + padding * 2}px`;
+	}
 }
