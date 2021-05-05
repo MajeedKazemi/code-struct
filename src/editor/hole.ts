90800026d7eddeee919fa9b01396a8e349b503e0
hayatpur <devamar.hayatpur@mail.utoronto.ca>
2021-05-05 07:36:53 -0400

    Added back red background on incorrect input

--- src/editor/hole.ts
+++ src/editor/hole.ts
@@ -1,35 +1,51 @@
-import { CallbackType, CodeConstruct, Callback } from '../syntax-tree/ast';
-import Editor from './editor';
+import { CallbackType, CodeConstruct, Callback } from "../syntax-tree/ast";
+import Editor from "./editor";
 
 export default class Hole {
-	element: HTMLDivElement;
-	editor: Editor;
-	code: CodeConstruct;
-	container: HTMLElement;
+    element: HTMLDivElement;
+    editor: Editor;
+    code: CodeConstruct;
+    container: HTMLElement;
 
-	constructor(editor: Editor, code: CodeConstruct) {
-		this.editor = editor;
-		this.code = code;
+    constructor(editor: Editor, code: CodeConstruct) {
+        this.editor = editor;
+        this.code = code;
 
-		// Dom element
-		const element = document.createElement('div');
-		element.classList.add('hole');
-		
-        this.container = document.querySelector('.monaco-scrollable-element.editor-scrollable.vs')
+        // Dom element
+        const element = document.createElement("div");
+        element.classList.add("hole");
+
+        this.container = document.querySelector(".monaco-scrollable-element.editor-scrollable.vs");
         this.container.append(element);
 
-		this.element = element;
-		const hole = this;
+        this.element = element;
+        const hole = this;
 
-        code.subscribe(CallbackType.delete, new Callback(() => {
-            hole.setTransform({x: 0, y: 0, width: 0, height: 0});
-            hole.element.remove();
-        }));
-        
-        code.subscribe(CallbackType.replace, new Callback(() => {
-            hole.setTransform({x: 0, y: 0, width: 0, height: 0});
-			hole.element.remove();
-        }));
+        code.subscribe(
+            CallbackType.delete,
+            new Callback(() => {
+                hole.setTransform({ x: 0, y: 0, width: 0, height: 0 });
+                hole.element.remove();
+            })
+        );
+
+        code.subscribe(
+            CallbackType.replace,
+            new Callback(() => {
+                hole.setTransform({ x: 0, y: 0, width: 0, height: 0 });
+                hole.element.remove();
+            })
+        );
+
+        code.subscribe(
+            CallbackType.fail,
+            new Callback(() => {
+                hole.element.style.background = `rgba(255, 0, 0, 0.06)`;
+                setTimeout(() => {
+                    hole.element.style.background = `rgba(255, 0, 0, 0)`;
+                }, 1000);
+            })
+        );
 
         function loop() {
             const bbox = editor.computeBoundingBox(code.getSelection());
@@ -44,13 +60,13 @@ export default class Hole {
         loop();
     }
 
-	setTransform(transform: { x: number; width: number; y: number; height: number }) {
-		const padding = 0;
+    setTransform(transform: { x: number; width: number; y: number; height: number }) {
+        const padding = 0;
 
-		this.element.style.top = `${transform.y + 5}px`;
-		this.element.style.left = `${transform.x - padding}px`;
+        this.element.style.top = `${transform.y + 5}px`;
+        this.element.style.left = `${transform.x - padding}px`;
 
-		this.element.style.width = `${transform.width + padding * 2}px`;
-		this.element.style.height = `${transform.height - 5 * 2}px`;
-	}
+        this.element.style.width = `${transform.width + padding * 2}px`;
+        this.element.style.height = `${transform.height - 5 * 2}px`;
+    }
 }
