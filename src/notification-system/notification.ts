f94f9b105b9c2b8e831d9a34b919cce0ef063025
Viktar Chyhir <vchigir12@gmail.com>
2021-05-12 14:23:30 -0400

    Add custom hover notification

--- src/notification-system/notification.ts
+++ src/notification-system/notification.ts
@@ -1,67 +1,83 @@
 import * as monaco from 'monaco-editor';
 import Editor from '../editor/editor';
-import {Range} from "monaco-editor";
 
+const hoverNotificationMaxWidth = 200;
+const hoverNotificationMaxHeight = 25;
 
-export class Notification {
-    hoverProvider: monaco.IDisposable;
-    highlightDecoration: string[];
+abstract class Notification{
     message: string;
     editor: Editor;
-    notificationRange: Range;
     index: number;
+    htmlElement: HTMLDivElement;
+    htmlTextElement: HTMLDivElement;
+}
 
+export class HoverNotification extends Notification{
+    htmlHoverElement: HTMLDivElement;
+    selection: monaco.Selection;
 
-    constructor(editor: Editor, range: Range, index: number = -1, styleClass: string = "", message: string = ""){
+    constructor(editor: Editor, selection: monaco.Selection){
+        super();
+
+        this.selection = selection;
         this.editor = editor;
-        this.notificationRange = range;
-        this.message = message;
-        this.index = index;
 
-        this.highlightDecoration = this.highlightRange(styleClass); 
-        this.hoverProvider = this.addHover(range, message);
+        //highlight box
+        this.htmlElement = document.createElement("div");
+        this.htmlElement.classList.add("hoverNotificationHighlight");
+        this.setHighlightBounds();
+
+        //hover box
+        this.htmlHoverElement = document.createElement("div");
+        this.htmlHoverElement.classList.add("hoverNotificationHover");
+        this.htmlHoverElement.style.visibility = "hidden";
+        this.setHoverBoxBounds();
+        this.setHoverEvent();
+        this.htmlElement.appendChild(this.htmlHoverElement);
+
+        //TODO: text
+        
+
+        document.querySelector(".lines-content.monaco-editor-background").appendChild(this.htmlElement);
     }
 
-    /**
-     * Highlight rectangular area within the editor.
-     * 
-     * @param startLineNumber line to begin highlight on
-     * @param startPos        left column position of the highlight
-     * @param endLineNumber   line to end the highlight on
-     * @param rightPos        right column position of the highlight
-     * @param styleClass      name of css class containing the style for the highlight
-     */
-    highlightRange(styleClass: string){
-        return this.editor.monaco.deltaDecorations([], [
-                                                {
-                                                    range: this.notificationRange,
-                                                    options: {
-                                                        isWholeLine: false,
-                                                        inlineClassName: styleClass}
-                                                }
-                                                ]
-                                            );
+    private setHighlightBounds(){
+        const transform = this.editor.computeBoundingBox(this.selection);
+
+        this.htmlElement.style.top = `${transform.y + 5}px`;
+        this.htmlElement.style.left = `${transform.x - 0}px`;
+
+        this.htmlElement.style.width = `${transform.width + 0 * 2}px`;
+        this.htmlElement.style.height = `${transform.height - 5 * 2}px`;
     }
 
-    addHover(notificationRange: Range, errorMsg: string){
-        return monaco.languages.registerHoverProvider('python', {
-            provideHover: function(model, position){
-                if(notificationRange.containsPosition(position)){
-                    return {
-                        range: notificationRange,
-                        contents: [
-                            { value: '**SOURCE**' },
-                            { value: '```html\n' + errorMsg + '\n```' }
-                        ]}
-                }
-                return null;
-            }
+    private setHoverBoxBounds(){
+        this.htmlHoverElement.style.top = `${-hoverNotificationMaxHeight}px`;
+        this.htmlHoverElement.style.left = `${(-hoverNotificationMaxWidth / 2)}px`;
+
+        this.htmlHoverElement.style.width = `${hoverNotificationMaxWidth}px`;
+        this.htmlHoverElement.style.height = `${hoverNotificationMaxHeight}px`;
+
+        this.moveWithinEditor();
+
+    }
+
+    private setHoverEvent(){
+        this.htmlElement.addEventListener("mouseenter", (e) => {
+            this.htmlHoverElement.style.visibility = "visible";
+        })
+
+        this.htmlElement.addEventListener("mouseleave", (e) => {
+            this.htmlHoverElement.style.visibility = "hidden";
         })
     }
 
-    dispose(){
-        this.hoverProvider.dispose();
-        this.editor.monaco.deltaDecorations(this.highlightDecoration, [{ range: new monaco.Range(1,1,1,1), options : { } }]
-      );
+    private moveWithinEditor(){
+        if(parseInt(this.htmlHoverElement.style.left) < 0){
+            this.htmlHoverElement.style.left = `${parseInt(this.htmlElement.style.left)+ parseInt(this.htmlHoverElement.style.left)}px`;
+        }
+        if(parseInt(this.htmlHoverElement.style.top) < 0){
+            this.htmlHoverElement.style.top = `${this.editor.computeCharHeight()}px`;
+        }
     }
 }
