e96087a371da10a93f589934e2f7426d600ef333
Viktar Chyhir <vchigir12@gmail.com>
2021-05-14 12:31:45 -0400

    Add warnings for general errors, object-method type mismatch and out-of-scope var reference

--- src/notification-system/error-msg-generator.ts
+++ src/notification-system/error-msg-generator.ts
@@ -3,7 +3,8 @@ import {ErrorType} from "./notification-system-controller"
 
 export enum ErrorMessage{
     default,
-    outOfScopeVarReference
+    outOfScopeVarReference,
+    methodCallObjectTypeMismatch
 
 }
 
@@ -31,9 +32,14 @@ export class ErrorMessageGenerator{
      */
     generateMsg(errMsgType: ErrorMessage, args: any) : string{
         let msg = ""
+
+        //TODO: This is not exactly good design for the args parameter, but it is too varied on what it needs to contain. Include documentation on how its memebrs are used in diff errors.
         switch(errMsgType){
             case ErrorMessage.outOfScopeVarReference:
                 msg = `Attempted to reference "${args.identifier}" out of scope.`
+                break;
+            case ErrorMessage.methodCallObjectTypeMismatch:
+                msg = `Attempted to call method from object that does not support it. Object type: ${args.objectType} -- Method: ${args.method} -- Called On: ${args.methodCalledOn}`
                 break;
             default:
                 msg = "Invalid action."
