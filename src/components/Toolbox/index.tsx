75b54bbfe59614a8f0756e16a08cbf96d4d8499b
Viktar Chyhir <vchigir12@gmail.com>
2021-08-10 09:27:57 -0400

    Fix toolbox hole styling

--- src/components/Toolbox/index.tsx
+++ src/components/Toolbox/index.tsx
@@ -13,19 +13,19 @@ class Toolbox extends React.Component<Props, State>{
       <div id = "mainToolboxDiv" className = {this.props.className}>
          <div className="group">
                     <p>Function calls</p>
-                    <div id="add-print-btn" className="button">print(<hole></hole>)</div>
-                    <div id="add-randint-btn" className="button">randint(<hole></hole>, <hole></hole>)</div>
-                    <div id="add-range-btn" className="button">range(<hole></hole>, <hole></hole>)</div>
-                    <div id="add-len-btn" className="button">len(<hole></hole>)</div>
+                    <div id="add-print-btn" className="button">print(<div className="hole"></div>)</div>
+                    <div id="add-randint-btn" className="button">randint(<div className="hole"></div>, <div className="hole"></div>)</div>
+                    <div id="add-range-btn" className="button">range(<div className="hole"></div>, <div className="hole"></div>)</div>
+                    <div id="add-len-btn" className="button">len(<div className="hole"></div>)</div>
                 </div>
 
                 <div className="group">
                     <p>Control flow</p>
-                    <div id="add-while-expr-btn" className="button">while <hole></hole> :</div>
-                    <div id="add-if-expr-btn" className="button">if <hole></hole> :</div>
-                    <div id="add-elif-expr-btn" className="button">elif <hole></hole> :</div>
+                    <div id="add-while-expr-btn" className="button">while <div className="hole"></div> :</div>
+                    <div id="add-if-expr-btn" className="button">if <div className="hole"></div> :</div>
+                    <div id="add-elif-expr-btn" className="button">elif <div className="hole"></div> :</div>
                     <div id="add-else-expr-btn" className="button">else:</div>
-                    <div id="add-for-expr-btn" className="button">for <hole></hole> in <hole></hole> :</div>
+                    <div id="add-for-expr-btn" className="button">for <div className="hole"></div> in <div className="hole"></div> :</div>
                 </div>
 
                 <div className="group">
@@ -38,7 +38,7 @@ class Toolbox extends React.Component<Props, State>{
 
                 <div className="group" id="variables">
                     <p>Variables</p>
-                    <div id="add-var-btn" className="button"><hole></hole> = <span className="hole-toolbox"></span></div>
+                    <div id="add-var-btn" className="button"><div className="hole"></div> = <span className="hole-toolbox"></span></div>
                 </div>
 
                 <div id="user-variables">
@@ -52,14 +52,14 @@ class Toolbox extends React.Component<Props, State>{
 
                     <div id="add-list-literal-btn" className="button">[]</div>
                     <div id="add-list-item-btn" className="button">
-                        , <hole></hole>
+                        , <div className="hole"></div>
                     </div>
                     <div id="add-list-append-stmt-btn" className="button">
-                        <span className="hole-toolbox"></span>.append(<hole></hole>)
+                        <span className="hole-toolbox"></span>.append(<div className="hole"></div>)
                     </div>
-                    <div id="add-list-index-btn" className="button"><hole></hole>[<hole></hole>]</div>
+                    <div id="add-list-index-btn" className="button"><div className="hole"></div>[<div className="hole"></div>]</div>
                     <div id="add-list-elem-assign-btn" className="button">
-                        <hole></hole>[<hole></hole>] = <span className="hole-toolbox"></span>
+                        <div className="hole"></div>[<div className="hole"></div>] = <span className="hole-toolbox"></span>
                     </div>
                 </div>
 
@@ -73,62 +73,62 @@ class Toolbox extends React.Component<Props, State>{
 
                 <div className="group">
                     <p>Casting</p>
-                    <div id="add-cast-str-btn" className="button">str(<hole></hole>)</div>
+                    <div id="add-cast-str-btn" className="button">str(<div className="hole"></div>)</div>
                 </div>
 
                 <div className="group">
                     <p>Operations</p>
                     <div id="add-bin-add-expr-btn" className="button">
-                        <hole></hole> + <span className="hole-toolbox"></span>
+                        <div className="hole"></div> + <span className="hole-toolbox"></span>
                     </div>
                     <div id="add-bin-sub-expr-btn" className="button">
-                        <hole></hole> - <span className="hole-toolbox"></span>
+                        <div className="hole"></div> - <span className="hole-toolbox"></span>
                     </div>
                     <div id="add-bin-mul-expr-btn" className="button">
-                        <hole></hole> * <span className="hole-toolbox"></span>
+                        <div className="hole"></div> * <span className="hole-toolbox"></span>
                     </div>
                     <div id="add-bin-div-expr-btn" className="button">
-                        <hole></hole> / <span className="hole-toolbox"></span>
+                        <div className="hole"></div> / <span className="hole-toolbox"></span>
                     </div>
                 </div>
 
                 <div className="group">
                     <p>Booleans</p>
                     <div id="add-bin-and-expr-btn" className="button">
-                        <hole></hole> and
-                        <hole></hole>
+                        <div className="hole"></div> and
+                        <div className="hole"></div>
                     </div>
                     <div id="add-bin-or-expr-btn" className="button">
-                        <hole></hole> or <span className="hole-toolbox"></span>
+                        <div className="hole"></div> or <span className="hole-toolbox"></span>
                     </div>
-                    <div id="add-unary-not-expr-btn" className="button">not <hole></hole></div>
+                    <div id="add-unary-not-expr-btn" className="button">not <div className="hole"></div></div>
                 </div>
 
                 <div className="group">
                     <p>Comparisons</p>
                     <div id="add-comp-eq-expr-btn" className="button">
-                        <hole></hole> ==
-                        <hole></hole>
+                        <div className="hole"></div> ==
+                        <div className="hole"></div>
                     </div>
                     <div id="add-comp-neq-expr-btn" className="button">
-                        <hole></hole> !=
-                        <hole></hole>
+                        <div className="hole"></div> !=
+                        <div className="hole"></div>
                     </div>
                     <div id="add-comp-lt-expr-btn" className="button">
-                        <hole></hole> &lt;
-                        <hole></hole>
+                        <div className="hole"></div> &lt;
+                        <div className="hole"></div>
                     </div>
                     <div id="add-comp-lte-expr-btn" className="button">
-                        <hole></hole> &lt;=
-                        <hole></hole>
+                        <div className="hole"></div> &lt;=
+                        <div className="hole"></div>
                     </div>
                     <div id="add-comp-gt-expr-btn" className="button">
-                        <hole></hole> &gt;
-                        <hole></hole>
+                        <div className="hole"></div> &gt;
+                        <div className="hole"></div>
                     </div>
                     <div id="add-comp-gte-expr-btn" className="button">
-                        <hole></hole> &gt;=
-                        <hole></hole>
+                        <div className="hole"></div> &gt;=
+                        <div className="hole"></div>
                     </div>
                 </div>
             </div>
