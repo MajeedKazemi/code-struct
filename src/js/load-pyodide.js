dd6023cf0b19e1e63bd6c9b831d0d7a541ae3a7e
MajeedKazemi <majeed@cs.toronto.edu>
2021-08-31 10:42:15 -0400

    cleaned pyodide code

--- src/js/load-pyodide.js
+++ src/js/load-pyodide.js
@@ -1,17 +1,13 @@
-const {addTextToConsole} = require("./pyodide-controller")
+const { addTextToConsole } = require("./pyodide-controller");
 
-export default new Promise(async $export => {
-    const module = await Promise.resolve(
-        {
-            pyodideController: loadPyodide(
-                { 
-                    indexURL : "https://cdn.jsdelivr.net/pyodide/v0.18.0/full/", 
-                    stdout: (text) => {
-                        addTextToConsole(text);
-                    }, 
-                }
-           )
-        }
-    );
+export default new Promise(async ($export) => {
+    const module = await Promise.resolve({
+        pyodideController: loadPyodide({
+            indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.0/full/",
+            stdout: (text) => {
+                addTextToConsole(text);
+            },
+        }),
+    });
     $export(module);
 });
