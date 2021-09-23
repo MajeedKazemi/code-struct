61f62039dca6248cb33da25b1ea04f84a9c4155c
MajeedKazemi <majeed@cs.toronto.edu>
2021-09-23 15:52:15 -0400

    changed the doc-box to be bounded by the screen

--- src/editor/doc-box.ts
+++ src/editor/doc-box.ts
@@ -6,11 +6,12 @@ export class DocumentationBox {
 
         document.body.appendChild(container);
 
-        function dragElement(elmnt) {
+        function dragElement(elmnt: HTMLDivElement) {
             var pos1 = 0,
                 pos2 = 0,
                 pos3 = 0,
                 pos4 = 0;
+
             if (document.getElementById(elmnt.id + "header")) {
                 // if present, the header is where you move the DIV from:
                 document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
@@ -22,10 +23,12 @@ export class DocumentationBox {
             function dragMouseDown(e) {
                 e = e || window.event;
                 e.preventDefault();
+
                 // get the mouse cursor position at startup:
                 pos3 = e.clientX;
                 pos4 = e.clientY;
                 document.onmouseup = closeDragElement;
+
                 // call a function whenever the cursor moves:
                 document.onmousemove = elementDrag;
             }
@@ -33,14 +36,27 @@ export class DocumentationBox {
             function elementDrag(e) {
                 e = e || window.event;
                 e.preventDefault();
+
                 // calculate the new cursor position:
                 pos1 = pos3 - e.clientX;
                 pos2 = pos4 - e.clientY;
                 pos3 = e.clientX;
                 pos4 = e.clientY;
-                // set the element's new position:
-                elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-                elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+
+                if (
+                    elmnt.offsetTop - pos2 > 0 &&
+                    window.innerHeight - (elmnt.offsetTop - pos2 + elmnt.clientHeight) > 0
+                ) {
+                    // set the element's new position:
+                    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+                }
+
+                if (
+                    elmnt.offsetLeft - pos1 > 0 &&
+                    window.innerWidth - (elmnt.offsetLeft - pos1 + elmnt.clientWidth) > 0
+                ) {
+                    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+                }
             }
 
             function closeDragElement() {
