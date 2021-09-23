c7d87d38f358eb8789bf1882c6228a7a22e020f2
MajeedKazemi <majeed@cs.toronto.edu>
2021-09-23 18:40:22 -0400

    added the close button and header to the doc-box

--- src/editor/doc-box.ts
+++ src/editor/doc-box.ts
@@ -4,68 +4,81 @@ export class DocumentationBox {
         container.classList.add("doc-box-container");
         container.id = uniqueId;
 
+        const headerDiv = document.createElement("div");
+        headerDiv.classList.add("doc-box-header");
+
+        const closeButton = document.createElement("div");
+        closeButton.classList.add("close-button");
+        closeButton.innerHTML = `<span>&times;</span>`;
+
+        closeButton.onclick = () => {
+            container.remove();
+        };
+
+        headerDiv.appendChild(closeButton);
+        container.appendChild(headerDiv);
+
         document.body.appendChild(container);
+        makeDraggable(headerDiv);
+    }
+}
 
-        function dragElement(elmnt: HTMLDivElement) {
-            var pos1 = 0,
-                pos2 = 0,
-                pos3 = 0,
-                pos4 = 0;
+function makeDraggable(element: HTMLDivElement) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
 
-            if (document.getElementById(elmnt.id + "header")) {
-                // if present, the header is where you move the DIV from:
-                document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-            } else {
-                // otherwise, move the DIV from anywhere inside the DIV:
-                elmnt.onmousedown = dragMouseDown;
-            }
+    if (document.getElementById(element.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(element.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        element.onmousedown = dragMouseDown;
+    }
 
-            function dragMouseDown(e) {
-                e = e || window.event;
-                e.preventDefault();
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
 
-                // get the mouse cursor position at startup:
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                document.onmouseup = closeDragElement;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
 
-                // call a function whenever the cursor moves:
-                document.onmousemove = elementDrag;
-            }
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
 
-            function elementDrag(e) {
-                e = e || window.event;
-                e.preventDefault();
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
 
-                // calculate the new cursor position:
-                pos1 = pos3 - e.clientX;
-                pos2 = pos4 - e.clientY;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
 
-                if (
-                    elmnt.offsetTop - pos2 > 0 &&
-                    window.innerHeight - (elmnt.offsetTop - pos2 + elmnt.clientHeight) > 0
-                ) {
-                    // set the element's new position:
-                    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-                }
-
-                if (
-                    elmnt.offsetLeft - pos1 > 0 &&
-                    window.innerWidth - (elmnt.offsetLeft - pos1 + elmnt.clientWidth) > 0
-                ) {
-                    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-                }
-            }
-
-            function closeDragElement() {
-                // stop moving when mouse button is released:
-                document.onmouseup = null;
-                document.onmousemove = null;
-            }
+        if (
+            element.parentElement.offsetTop - pos2 > 0 &&
+            window.innerHeight - (element.parentElement.offsetTop - pos2 + element.parentElement.clientHeight) > 0
+        ) {
+            // set the element's new position:
+            element.parentElement.style.top = element.parentElement.offsetTop - pos2 + "px";
         }
 
-        dragElement(container);
+        if (
+            element.parentElement.offsetLeft - pos1 > 0 &&
+            window.innerWidth - (element.parentElement.offsetLeft - pos1 + element.parentElement.clientWidth) > 0
+        ) {
+            element.parentElement.style.left = element.parentElement.offsetLeft - pos1 + "px";
+        }
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
 }
