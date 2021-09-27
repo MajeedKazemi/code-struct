5e43c0d9d0d932b93dd0810b3a9f3e0e4278c85a
MajeedKazemi <majeed@cs.toronto.edu>
2021-09-27 14:28:52 -0400

    updated mini example editor - added a header and a output box - added run/clear button - added reset button

--- src/editor/doc-box.ts
+++ src/editor/doc-box.ts
@@ -1,6 +1,8 @@
 import { editor } from "monaco-editor";
 
 export class DocumentationBox {
+    private static exampleCounter = 0;
+
     constructor(uniqueId: string, documentation: any) {
         const container = document.createElement("div");
         container.classList.add("doc-box-container");
@@ -17,6 +19,11 @@ export class DocumentationBox {
             container.remove();
         };
 
+        const docTitle = document.createElement("h3");
+        docTitle.classList.add("doc-title");
+        docTitle.innerText = documentation.title;
+        headerDiv.appendChild(docTitle);
+
         headerDiv.appendChild(closeButton);
         container.appendChild(headerDiv);
 
@@ -28,10 +35,6 @@ export class DocumentationBox {
         docBody.classList.add("doc-body");
         container.appendChild(docBody);
 
-        const docTitle = document.createElement("h3");
-        docTitle.innerText = documentation.title;
-        docBody.appendChild(docTitle);
-
         for (const item of documentation.body) {
             if (item.hasOwnProperty("paragraph")) {
                 const p = document.createElement("p");
@@ -39,29 +42,93 @@ export class DocumentationBox {
 
                 docBody.appendChild(p);
             } else if (item.hasOwnProperty("example")) {
-                const exampleEditor = document.createElement("div");
-                exampleEditor.classList.add("doc-editor");
-
-                docBody.appendChild(exampleEditor);
-
-                editor.create(exampleEditor, {
-                    value: item.example,
-                    language: "python",
-                    dimension: { width: 200, height: 100 },
-                    minimap: {
-                        enabled: false,
-                    },
-                    automaticLayout: true,
-                    scrollbar: {
-                        vertical: "auto",
-                        horizontal: "auto",
-                    },
-                    fontSize: 14,
-                    lineHeight: 20,
-                });
+                docBody.appendChild(createExample(item));
             }
         }
     }
+
+    static getNewConsoleId(): string {
+        return "console-id-" + DocumentationBox.exampleCounter++;
+    }
+}
+
+function createExample(item): HTMLDivElement {
+    const editorContainer = document.createElement("div");
+    editorContainer.classList.add("doc-editor-container");
+
+    const editorHeader = document.createElement("div");
+    editorHeader.innerText = "Example Code";
+    editorHeader.classList.add("doc-editor-header");
+    editorContainer.appendChild(editorHeader);
+
+    const exampleEditor = document.createElement("div");
+    exampleEditor.classList.add("doc-editor");
+    editorContainer.appendChild(exampleEditor);
+
+    const exampleConsole = document.createElement("div");
+    exampleConsole.classList.add("doc-example-console");
+    editorContainer.appendChild(exampleConsole);
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("doc-example-console-button-container");
+    exampleConsole.appendChild(buttonContainer);
+
+    const consoleId = DocumentationBox.getNewConsoleId();
+
+    const runButton = document.createElement("div");
+    runButton.innerText = "> Run";
+    runButton.id = `run-${consoleId}`;
+    runButton.classList.add(...["console-button", "run-code-btn", "doc-example-btn"]);
+    buttonContainer.appendChild(runButton);
+
+    const clearConsoleButton = document.createElement("div");
+    clearConsoleButton.innerText = "Clear";
+    clearConsoleButton.id = `clear-${consoleId}`;
+    clearConsoleButton.classList.add(...["doc-example-btn", "console-button", "clear-output-btn"]);
+    buttonContainer.appendChild(clearConsoleButton);
+
+    const resetConsoleButton = document.createElement("div");
+    resetConsoleButton.innerText = "Reset";
+    resetConsoleButton.id = `reset-${consoleId}`;
+    resetConsoleButton.classList.add(...["doc-example-btn", "console-button", "reset-editor-btn"]);
+    buttonContainer.appendChild(resetConsoleButton);
+
+    const consoleOutput = document.createElement("div");
+    consoleOutput.id = `console-output-${consoleId}`;
+    consoleOutput.classList.add("console-output");
+    exampleConsole.appendChild(consoleOutput);
+
+    const codeEditor = editor.create(exampleEditor, {
+        value: item.example,
+        language: "python",
+        dimension: { width: 200, height: 100 },
+        minimap: {
+            enabled: false,
+        },
+        overviewRulerLanes: 0,
+        overviewRulerBorder: false,
+        contextmenu: false,
+        mouseWheelScrollSensitivity: 0,
+        automaticLayout: true,
+        scrollbar: {
+            vertical: "auto",
+            horizontal: "auto",
+            verticalSliderSize: 5,
+            scrollByPage: false,
+        },
+        fontSize: 13,
+        lineHeight: 19,
+    });
+
+    codeEditor.onDidChangeModelContent(() => {
+        resetConsoleButton.style.visibility = "visible";
+    });
+
+    resetConsoleButton.addEventListener("click", () => {
+        codeEditor.setValue(item.example);
+    });
+
+    return editorContainer;
 }
 
 function makeDraggable(element: HTMLDivElement) {
