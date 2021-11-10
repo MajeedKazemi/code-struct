1fdfa74493505dc66445a613b8b18a9e2f5a9319
MajeedKazemi <majeed@cs.toronto.edu>
2021-11-10 12:19:30 -0500

    added f-string support to the language tokenizer

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,4 +1,4 @@
-import { editor, Range, Selection } from "monaco-editor";
+import { editor, languages, Range, Selection } from "monaco-editor";
 import { CodeConstruct, EditableTextTkn, IdentifierTkn, Statement, TypedEmptyExpr } from "../syntax-tree/ast";
 import { TAB_SPACES } from "../syntax-tree/consts";
 import { Module } from "../syntax-tree/module";
@@ -18,11 +18,249 @@ export class Editor {
     mousePosMonaco: any;
 
     constructor(parentEl: HTMLElement, module: Module) {
+        languages.register({ id: "python3.6" });
+        languages.setMonarchTokensProvider("python3.6", {
+            defaultToken: "",
+            tokenPostfix: ".python",
+
+            keywords: [
+                "and",
+                "as",
+                "assert",
+                "break",
+                "class",
+                "continue",
+                "def",
+                "del",
+                "elif",
+                "else",
+                "except",
+                "exec",
+                "finally",
+                "for",
+                "from",
+                "global",
+                "if",
+                "import",
+                "in",
+                "is",
+                "lambda",
+                "None",
+                "not",
+                "or",
+                "pass",
+                "print",
+                "raise",
+                "return",
+                "self",
+                "try",
+                "while",
+                "with",
+                "yield",
+
+                "int",
+                "float",
+                "long",
+                "complex",
+                "hex",
+
+                "abs",
+                "all",
+                "any",
+                "apply",
+                "basestring",
+                "bin",
+                "bool",
+                "buffer",
+                "bytearray",
+                "callable",
+                "chr",
+                "classmethod",
+                "cmp",
+                "coerce",
+                "compile",
+                "complex",
+                "delattr",
+                "dict",
+                "dir",
+                "divmod",
+                "enumerate",
+                "eval",
+                "execfile",
+                "file",
+                "filter",
+                "format",
+                "frozenset",
+                "getattr",
+                "globals",
+                "hasattr",
+                "hash",
+                "help",
+                "id",
+                "input",
+                "intern",
+                "isinstance",
+                "issubclass",
+                "iter",
+                "len",
+                "locals",
+                "list",
+                "map",
+                "max",
+                "memoryview",
+                "min",
+                "next",
+                "object",
+                "oct",
+                "open",
+                "ord",
+                "pow",
+                "print",
+                "property",
+                "reversed",
+                "range",
+                "raw_input",
+                "reduce",
+                "reload",
+                "repr",
+                "reversed",
+                "round",
+                "set",
+                "setattr",
+                "slice",
+                "sorted",
+                "staticmethod",
+                "str",
+                "sum",
+                "super",
+                "tuple",
+                "type",
+                "unichr",
+                "unicode",
+                "vars",
+                "xrange",
+                "zip",
+
+                "True",
+                "False",
+
+                "__dict__",
+                "__methods__",
+                "__members__",
+                "__class__",
+                "__bases__",
+                "__name__",
+                "__mro__",
+                "__subclasses__",
+                "__init__",
+                "__import__",
+            ],
+
+            escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
+            brackets: [
+                { open: "{", close: "}", token: "delimiter.curly" },
+                { open: "[", close: "]", token: "delimiter.bracket" },
+                { open: "(", close: ")", token: "delimiter.parenthesis" },
+            ],
+
+            tokenizer: {
+                root: [
+                    { include: "@whitespace" },
+                    { include: "@numbers" },
+                    { include: "@strings" },
+
+                    [/[,:;]/, "delimiter"],
+                    [/[{}\[\]()]/, "@brackets"],
+
+                    [/@[a-zA-Z]\w*/, "tag"],
+                    [
+                        /[a-zA-Z]\w*/,
+                        {
+                            cases: {
+                                "@keywords": "keyword",
+                                "@default": "identifier",
+                            },
+                        },
+                    ],
+                ],
+
+                // Deal with white space, including single and multi-line comments
+                whitespace: [
+                    [/\s+/, "white"],
+                    [/(^#.*$)/, "comment"],
+                    [/('''.*''')|(""".*""")/, "string"],
+                    [/'''.*$/, "string", "@endDocString"],
+                    [/""".*$/, "string", "@endDblDocString"],
+                ],
+                endDocString: [
+                    [/\\'/, "string"],
+                    [/.*'''/, "string", "@popall"],
+                    [/.*$/, "string"],
+                ],
+                endDblDocString: [
+                    [/\\"/, "string"],
+                    [/.*"""/, "string", "@popall"],
+                    [/.*$/, "string"],
+                ],
+
+                // Recognize hex, negatives, decimals, imaginaries, longs, and scientific notation
+                numbers: [
+                    [/-?0x([abcdef]|[ABCDEF]|\d)+[lL]?/, "number.hex"],
+                    [/-?(\d*\.)?\d+([eE][+\-]?\d+)?[jJ]?[lL]?/, "number"],
+                ],
+
+                // Recognize strings, including those broken across lines with \ (but not without)
+                strings: [
+                    [/'$/, "string.escape", "@popall"],
+                    [/'/, "string.escape", "@stringBody"],
+                    [/"/, "string", "@string_double"],
+                    [/"/, "string.escape", "@dblStringBody"],
+                    [/f'/, "string", "@string_backtick"],
+                ],
+                stringBody: [
+                    [/[^\\']+$/, "string", "@popall"],
+                    [/[^\\']+/, "string"],
+                    [/\\./, "string"],
+                    [/'/, "string.escape", "@popall"],
+                    [/\\$/, "string"],
+                ],
+                dblStringBody: [
+                    [/[^\\"]+$/, "string", "@popall"],
+                    [/[^\\"]+/, "string"],
+                    [/\\./, "string"],
+                    [/"/, "string.escape", "@popall"],
+                    [/\\$/, "string"],
+                ],
+
+                string_double: [
+                    [/[^\\"]+/, "string"],
+                    [/\\./, "string.escape.invalid"],
+                    [/"/, "string", "@pop"],
+                ],
+
+                string_backtick: [
+                    [/\{/, { token: "delimiter.curly", next: "@bracketCounting" }],
+                    [/[^\\']/, "string"],
+                    [/@escapes/, "string.escape"],
+                    [/\\./, "string.escape.invalid"],
+                    [/'/, "string", "@pop"],
+                ],
+
+                bracketCounting: [
+                    [/\{/, "delimiter.curly", "@bracketCounting"],
+                    [/\}/, "delimiter.curly", "@pop"],
+                    { include: "root" },
+                ],
+            },
+        });
+
         this.monaco = editor.create(parentEl, {
             folding: false,
             dimension: { height: 500, width: 700 },
             value: "",
-            language: "python",
+            language: "python3.6",
+            theme: "vs",
             minimap: {
                 enabled: false,
             },
